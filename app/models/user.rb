d81d55fc6a639a32379b752c3758988ef21b6aa3
Roxane Haddad <roxane.haddad@gmail.com>
2020-02-24 18:30:35 +0100

    made some changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :flats, :rentings, :visits, dependent: :destroy
+  has_many :flats, dependent: :destroy
+  has_many :rentings, dependent: :destroy
+  has_many :visits, dependent: :destroy
   has_many :schedules, through: :flats
   has_many :documents, through: :rentings
-  validates :email, :phone_number uniqueness: true
+  validates :email, :phone_number, uniqueness: true
   validates :first_name, :last_name, :phone_number, :email, presence: true
 end
