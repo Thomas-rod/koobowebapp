af7a1072721c5cf757e2bcb4df976d393df0ddfb
Roxane Haddad <roxane.haddad@gmail.com>
2020-02-25 17:55:00 +0100

    finished the form with minimal formatting

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,12 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+// require ("select2_simple_form")
+import 'select2/dist/css/select2.css';
 
+import { initSelect2 } from '../components/init_select2';
+
+initSelect2();
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
