a241f19a702b9db459ebbf8d4d3f1383ee15c802
Roxane Haddad <roxane.haddad@gmail.com>
2020-03-01 21:38:08 +0100

    ... to be continued

--- app/javascript/components/checkboxes.js
+++ app/javascript/components/checkboxes.js
@@ -1,6 +1,6 @@
 const checkboxesjs = () => {
   $(document).ready(function(){
-    $(".radio-part").click(function(){
+    $(".form-part.radio-part").click(function(){
       $(this).toggleClass("active");
     });
   });
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -41,6 +41,7 @@ document.addEventListener('turbolinks:load', () => {
   initSelect2();
   clickableSchedule();
   initAutocomplete();
+  checkboxesjs();
 });
 
 
