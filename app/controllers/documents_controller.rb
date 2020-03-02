a69348f2b38406ebeea5f8559491f5fcb4010206
thomas rodier <rodiert17@gmail.com>
2020-03-02 12:17:01 +0100

    add documents stuff

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -1,2 +1,21 @@
 class DocumentsController < ApplicationController
+
+  def index
+    @documents = Document.all
+    @flats_rented = flats_rented
+    @rentings = Renting.all
+  end
+
+  private
+
+  def flats_rented
+    flats_rented = []
+    flats = Flat.all
+    flats.each do |flat|
+      if !flat.rentings.empty?
+        flats_rented << flat
+      end
+    end
+    flats_rented
+  end
 end
