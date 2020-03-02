b273d5e807e5a5b1964ebb6bdd3fe285c8ddce97
Roxane Haddad <roxane.haddad@gmail.com>
2020-03-02 16:11:15 +0100

    finished the preview carousellll

--- app/javascript/components/photo_preview.js
+++ app/javascript/components/photo_preview.js
@@ -24,16 +24,15 @@ const displayPreview = (input) => {
     }
     reader2.readAsDataURL(input.files[1])
     document.getElementById('photo-preview2').classList.remove('hidden');
-    document.querySelector(".hide-carousel").classList.remove('hide-carousel')
   }
   if (input.files && input.files[2]) {
+    console.log(input.files[2])
     const reader3 = new FileReader();
-    reader2.onload = (event) => {
+    reader3.onload = (event) => {
       document.getElementById('photo-preview3').src = event.currentTarget.result;
     }
     reader3.readAsDataURL(input.files[2])
     document.getElementById('photo-preview3').classList.remove('hidden');
-    document.querySelector(".hide-carousel").classList.remove('hide-carousel')
   }
 }
 
