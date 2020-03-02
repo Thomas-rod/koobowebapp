6e0f3de4cbac9b7cb8a5199652f162b575cbb023
Jonathan Courdavault <jo.courdavault@gmail.com>
2020-03-02 18:53:37 +0100

    notif done, 404 and 500 custumized

--- app/javascript/channels/user_channel.js
+++ app/javascript/channels/user_channel.js
@@ -8,6 +8,10 @@ if (notificationsContainer) {
   consumer.subscriptions.create({ channel: "UserChannel", id: id }, {
     received(data) {
       notifContainer.insertAdjacentHTML('beforeend', data);
+      const counterBell = document.getElementById('bell-counter');
+      counterBell.innerHTML = parseInt(counterBell.innerHTML) + 1;
+      const dashboardCounter = document.getElementById('notif-counter');
+      dashboardCounter.innerHTML = parseInt(dashboardCounter.innerHTML + 1);
     },
   });
 }
--- app/javascript/components/banner.js
+++ app/javascript/components/banner.js
@@ -1,13 +1,14 @@
 import Typed from 'typed.js';
 
 const loadDynamicBannerText = () => {
-  new Typed('#banner-typed-text', {
-    strings: ["facile", "simple", "rapide",""],
-    typeSpeed: 150,
-    loop: true,
-    showCursor: false,
-    fadeOut: true
-  });
+  if (document.querySelector('#banner-typed-text')){
+    new Typed('#banner-typed-text', {
+      strings: ["facile", "simple", "rapide",""],
+      typeSpeed: 150,
+      loop: true,
+      showCursor: false,
+      fadeOut: true
+    });
+  }
 }
-
 export { loadDynamicBannerText };
