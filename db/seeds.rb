b87f2c8127680251e37cb46c3684a147156909a4
Roxane Haddad <roxane.haddad@gmail.com>
2020-03-04 13:54:53 +0100

    new seeds done

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,116 +10,11 @@ require 'open-uri'
 require 'json'
 require 'faker'
 
-  # url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
-  # ingredients_serialized = open(url).read
-  # ingredients = JSON.parse(ingredients_serialized)
-puts "cleaning Users, Flats, Schedules"
-User.destroy_all
-Flat.destroy_all
-Schedule.destroy_all
-Renting.destroy_all
-Visit.destroy_all
-puts "table flats shedules user have been deleted"
-
-def attach_photo_user(string, user)
-  file = URI.open(string)
-  user.photo.attach(io: file, filename: 'nes.png', content_type: 'image/png')
-end
-def attach_photo_flat(string, flat)
-  file = URI.open(string)
-  flat.photos.attach(io: file, filename: 'nes.png', content_type: 'image/png')
-end
-
-puts "creating User"
-
-roxane = User.new(first_name: "Roxane", last_name: "Haddad", phone_number: "0664784489", email: "roxane.haddad@gmail.com", password: "roxane.haddad@gmail.com")
-attach_photo_user("https://avatars0.githubusercontent.com/u/54777799?v=4", roxane)
-roxane.save
-john = User.new(first_name: "Jonathan", last_name: "Courdavault", phone_number: "0778381974", email: "jo.courdavault@gmail.com", password: "jo.courdavault@gmail.com")
-attach_photo_user("https://avatars2.githubusercontent.com/u/58211236?v=4", john)
-john.save
-thomas = User.new(first_name: "Thomas", last_name: "Rodier", phone_number: "0668489169", email: "rodiert17@gmail.com", password: "rodiert17@gmail.com")
-attach_photo_user("https://avatars1.githubusercontent.com/u/57214511?v=4", thomas)
-thomas.save
-mao = User.new(first_name: "Maodo", last_name: "Diop", phone_number: "0663906049", email: "maodod1@gmail.com", password: "maodod1@gmail.com", description: 'Best cooker in the world !! ')
-attach_photo_user("https://avatars1.githubusercontent.com/u/56120487?v=4", mao)
-mao.save
-puts "#{User.count} users have beeen created"
-
-puts "Flats seeding..."
-
-roxane_flat = Flat.new(user: roxane, address: "4 rue des Prêtres Saint-Séverin, 75005, Paris", description: "Quartier central et animé, appartement cosy avec magnifique vue.", monthly_price: 783, visible: true, rented: false, name: "Haddad House", number_of_rooms: 2, number_of_bedrooms: 1, surface: 23, floor: 5, elevator: true, balcony: false, cellar: true, parking: false, heating_system: ["", "individuel", "électrique"], furnished: false)
-attach_photo_flat("https://images.unsplash.com/photo-1550581190-9c1c48d21d6c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60", roxane_flat)
-attach_photo_flat("https://images.unsplash.com/photo-1522771739844-6a9f6d5f14af?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60", roxane_flat)
-attach_photo_flat("https://images.unsplash.com/flagged/photo-1573168710465-7f7da9a23a15?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60", roxane_flat)
-roxane_flat.save
-mao_flat = Flat.new(user: roxane, address: "12 rue Madeleine Laffitte, 93100, Montreuil", description: "Superbe pavillon, parfait pour une famille aimant cuisiner.", monthly_price: 2200, visible: true, rented: false, name: "Diop House", number_of_rooms: 7, number_of_bedrooms: 5, surface: 80, floor: 3, elevator: false, balcony: true, cellar: true, parking: true, heating_system: ["", "collectif", "gaz"], furnished: false)
-attach_photo_flat("https://images.unsplash.com/photo-1464890100898-a385f744067f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60", mao_flat)
-attach_photo_flat("https://images.unsplash.com/photo-1451153378752-16ef2b36ad05?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60", mao_flat)
-mao_flat.save
-
-
-puts "#{roxane.flats.count} flats have been created"
-puts "Now we creating 4 Shedules by flats"
-puts "........."
-
-s1 = Schedule.create!(start:Time.new(2020, 3, 3, 8, 30, 0),end:Time.new(2020, 3, 3, 9, 0, 0),flat_id:roxane.flats.last.id)
-s2 = Schedule.create!(start:Time.new(2020, 3, 3, 9, 0, 0),end:Time.new(2020, 3, 3, 9, 30, 0),flat_id:roxane.flats.last.id)
-s3 = Schedule.create!(start:Time.new(2020, 3, 4, 9, 30, 0),end:Time.new(2020, 3, 4, 10, 0, 0),flat_id:roxane.flats.last.id)
-
-# Schedule.create!(start:Time.new(2020, 3, 2, 13, 0, 0),end:Time.new(2020, 3, 2, 13, 30, 0),flat_id:roxane.flats.last.id)
-# Schedule.create!(start:Time.new(2020, 3, 2, 13, 30, 0),end:Time.new(2020, 3, 2, 14, 0, 0),flat_id:roxane.flats.last.id)
-# Schedule.create!(start:Time.new(2020, 3, 2, 14, 0, 0),end:Time.new(2020, 3, 2, 14, 30, 0),flat_id:roxane.flats.last.id)
-# Schedule.create!(start:Time.new(2020, 3, 2, 14, 30, 0),end:Time.new(2020, 3, 2, 15, 0, 0),flat_id:roxane.flats.last.id)
-
-# thomas.flats.each do |flat|
-#  Schedule.create!(start:Time.new(2020, 3, 2, 7, 0, 0),end:Time.new(2020, 3, 2, 7, 30, 0),flat_id:flat.id)
-#  Schedule.create!(start:Time.new(2020, 3, 2, 8, 30, 0),end:Time.new(2020, 3, 2, 9, 0, 0),flat_id:flat.id)
-#  Schedule.create!(start:Time.new(2020, 3, 2, 9, 0, 0),end:Time.new(2020, 3, 2, 9, 30, 0),flat_id:flat.id)
-#  Schedule.create!(start:Time.new(2020, 3, 2, 9, 30, 0),end:Time.new(2020, 3, 2, 10, 0, 0),flat_id:flat.id)
-# end
-# mao.flats.each do |flat|
-#  Schedule.create!(start:Time.new(2020, 3, 2, 17, 0, 0),end:Time.new(2020, 3, 2, 17, 30, 0),flat_id:flat.id)
-#  Schedule.create!(start:Time.new(2020, 3, 2, 17, 30, 0),end:Time.new(2020, 3, 2, 18, 0, 0),flat_id:flat.id)
-#  Schedule.create!(start:Time.new(2020, 3, 2, 18, 0, 0),end:Time.new(2020, 3, 2, 18, 30, 0),flat_id:flat.id)
-#  Schedule.create!(start:Time.new(2020, 3, 2, 18, 30, 0),end:Time.new(2020, 3, 2, 19, 0, 0),flat_id:flat.id)
-# end
-
-renting_mao_roxane = Renting.create!(flat: roxane_flat, user: mao, created_at: 'Fri, 28 Dec 2019 14:27:32 UTC +00:00')
-
-puts "Now we are doing visits !"
-
-# Schedule.all.each do |schedule|
-
-#   2.times do
-#     Visit.create!(schedule: schedule, user: thomas, status: 'denied')
-#   end
-# end
-
-Visit.create!(schedule: s1, user:thomas, status: 'pending')
-Visit.create!(schedule: s1, user:mao, status: 'pending')
-
-Visit.create!(schedule: s3, user:thomas, status: 'pending')
-Visit.create!(schedule: s3, user:mao , status: 'pending')
-Visit.create!(schedule: s3, user:john, status: 'pending')
-Visit.create!(schedule: s3, user:roxane, status: 'pending')
-
-Visit.create!(schedule: s3, user:thomas, status: 'pending')
-Visit.create!(schedule: s3, user:mao , status: 'pending')
-
-
-puts "you have now #{Visit.count} visits"
-puts "you have now 4 schedules by flats "
-puts "total schedules = #{Schedule.count}"
-puts "G O O D     L U C K      G U Y S"
-
-
 puts "Cleaning DB"
 User.destroy_all
 Flat.destroy_all
 Schedule.destroy_all
 Renting.destroy_all
-Visit.destroy_all
 puts "All clean"
 
 def attach_photo_user(string, user)
@@ -144,7 +39,7 @@ thomas.save
 mao = User.new(first_name: "Maodo", last_name: "Diop", phone_number: "0663906049", email: "maodod1@gmail.com", password: "maodod1@gmail.com")
 attach_photo_user("https://avatars1.githubusercontent.com/u/56120487?v=4", mao)
 mao.save
-puts "#{User.count} created"
+puts "#{User.count} users created"
 
 puts "Creating flats"
 
@@ -153,18 +48,22 @@ attach_photo_flat("https://images.unsplash.com/photo-1550581190-9c1c48d21d6c?ixl
 attach_photo_flat("https://images.unsplash.com/photo-1522771739844-6a9f6d5f14af?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60", thomas_first_flat)
 attach_photo_flat("https://images.unsplash.com/flagged/photo-1573168710465-7f7da9a23a15?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60", thomas_first_flat)
 thomas_first_flat.save
-thomas_second_flat = Flat.new(user: thomas, address: "30 rue Broca, 75005, Paris", description: "Superbe studio très bien aménagé dans un quartier animé de la Rive Gauche.", monthly_price: 750, visible: true, rented: false, name: "Studio meublé Paris 13", number_of_rooms: 1, number_of_bedrooms: 1, surface: 25, floor: 5, elevator: false, balcony: false, cellar: true, parking: false, heating_system: ["", "individuel", "électrique"], furnished: true)
-attach_photo_flat("https://images.unsplash.com/photo-1464890100898-a385f744067f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60", thomas_second_flat)
+thomas_second_flat = Flat.new(user: thomas, address: "30 rue Broca, 75005, Paris", description: "Superbe studio très bien aménagé dans un quartier animé de la Rive Gauche.", monthly_price: 750, visible: true, rented: false, seloger: true, bienici: false, pap: true, leboncoin: true, name: "Studio meublé Paris 13", number_of_rooms: 1, number_of_bedrooms: 1, surface: 25, floor: 5, elevator: false, balcony: false, cellar: true, parking: false, heating_system: ["", "individuel", "électrique"], furnished: true)
+attach_photo_flat("https://images.unsplash.com/photo-1505691938895-1758d7feb511?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80", thomas_second_flat)
 attach_photo_flat("https://images.unsplash.com/photo-1451153378752-16ef2b36ad05?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60", thomas_second_flat)
+attach_photo_flat("https://images.unsplash.com/photo-1552454799-ca5cfdc612c8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=967&q=80", thomas_second_flat)
 thomas_second_flat.save
-puts "#{Flat.count} created"
+puts "#{Flat.count} flats created"
 
 puts "Creating schedules"
 first_schedule = Schedule.create!(start:Time.new(2020, 3, 3, 8, 30, 0),end:Time.new(2020, 3, 3, 9, 0, 0),flat: thomas_second_flat)
-second_schedule = Schedule.create!(start:Time.new(2020, 3, 3, 9, 0, 0),end:Time.new(2020, 3, 3, 9, 30, 0),flat_id: thomas_second_flat)
-third_schedule = Schedule.create!(start:Time.new(2020, 3, 4, 9, 30, 0),end:Time.new(2020, 3, 4, 10, 0, 0),flat_id: thomas_second_flat)
+second_schedule = Schedule.create!(start:Time.new(2020, 3, 3, 9, 0, 0),end:Time.new(2020, 3, 3, 9, 30, 0),flat: thomas_second_flat)
+third_schedule = Schedule.create!(start:Time.new(2020, 3, 4, 9, 30, 0),end:Time.new(2020, 3, 4, 10, 0, 0),flat: thomas_second_flat)
 puts "#{Schedule.count} created"
 
 puts "Creating renting"
 roxane_thomas_renting = Renting.create!(flat: thomas_first_flat, user: roxane, created_at: 'Fri, 28 Dec 2019 14:27:32 UTC +00:00')
-puts "#{Renting.count} created"
+puts "#{Renting.count} renting created"
+
+puts "G O O D     L U C K      G U Y S"
+
