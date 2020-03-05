65fb14d4c2bffa931b78c3ed7c31b9739aaceee4
Roxane Haddad <roxane.haddad@gmail.com>
2020-03-05 10:15:59 +0100

    put address on next line in the form

--- app/javascript/components/multi_etaps_form.js
+++ app/javascript/components/multi_etaps_form.js
@@ -4,41 +4,42 @@ import 'select2';
 const makeFormAppear = () => {
   const input1 = document.getElementById("my-name-input");
   const input2 = document.querySelector(".my-address-input");
-  const input4 = document.getElementById("my-rent-input");
-  const input3 = document.getElementById("my-size-input");
+  const input8 = document.getElementById("my-floor-input");
+  const input3 = document.getElementById("my-rent-input");
+  const input4 = document.getElementById("my-size-input");
   const input5 = document.getElementById("my-bedroom-input");
   const input6 = document.getElementById("my-heating-input");
   const input7 = document.getElementById("my-description-input");
 
   if (input1) {
     input1.addEventListener('change', () => {
-      displayNextLine(input1, input2, input3, input4, input5, input6, input7);
+      displayNextLine(input1, input2, input3, input4, input5, input6, input7, input8);
     })
   }
   if (input2) {
     input2.addEventListener('change', () => {
-      displayNextLine(input1, input2, input3, input4, input5, input6, input7);
+      displayNextLine(input1, input2, input3, input4, input5, input6, input7, input8);
     })
   }
   if (input3) {
     input3.addEventListener('change', () => {
-      displayNextLine(input1, input2, input3, input4, input5, input6, input7);
+      displayNextLine(input1, input2, input3, input4, input5, input6, input7, input8);
     })
   }
   if (input4) {
     input4.addEventListener('change', () => {
-      displayNextLine(input1, input2, input3, input4, input5, input6, input7);
+      displayNextLine(input1, input2, input3, input4, input5, input6, input7, input8);
     })
   }
   if (input5) {
     input5.addEventListener('change', () => {
-      displayNextLine(input1, input2, input3, input4, input5, input6, input7);
+      displayNextLine(input1, input2, input3, input4, input5, input6, input7, input8);
     })
   }
   if (input6) {
     $('#my-heating-input').on('select2:select', function (e) {
       const data = e.params.data;
-      displayNextLine(input1, input2, input3, input4, input5, input6, input7);
+      displayNextLine(input1, input2, input3, input4, input5, input6, input7, input8);
     });
   }
 
@@ -50,17 +51,26 @@ const makeFormAppear = () => {
 
   if (input7) {
     input7.addEventListener('change', () => {
-      displayNextLine(input1, input2, input3, input4, input5, input6, input7);
+      displayNextLine(input1, input2, input3, input4, input5, input6, input7, input8);
     });
+  }
+  if (input8) {
+    input8.addEventListener('change', () => {
+      displayNextLine(input1, input2, input3, input4, input5, input6, input7, input8);
+    })
   }
 }
 
-const displayNextLine = (input1, input2, input3, input4, input5, input6, input7) => {
+const displayNextLine = (input1, input2, input3, input4, input5, input6, input7, input8) => {
   if (input1.value !== "") {
     document.querySelector(".line-2").classList.remove('hide-carousel')
   }
   if (input2.value !== "") {
     document.querySelector(".line-3").classList.remove('hide-carousel')
+  }
+  if (input8.value !== "") {
+    console.log(input8.value)
+    document.querySelector(".line-12").classList.remove('hide-carousel')
   }
   if (input3.value !== "") {
     document.querySelector(".line-4").classList.remove('hide-carousel')
@@ -76,7 +86,7 @@ const displayNextLine = (input1, input2, input3, input4, input5, input6, input7)
     document.querySelector(".line-8").classList.remove('hide-carousel')
   }
   if (input7.value !== "") {
-    document.querySelector(".line-10").classList.remove('hide-carousel')
+    document.querySelector(".line-9").classList.remove('hide-carousel')
   }
 
 }
