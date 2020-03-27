8ee0f6f086a8894e97420a25d7aa9d944516a7a5
Roxane Haddad <roxane.haddad@gmail.com>
2020-03-27 17:44:26 +0100

    trying to resolve heroku issue

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,8 @@ module Koobowebapp
     config.i18n.default_locale = :fr
     config.time_zone = "Paris"
 
+    config.assets.initialize_on_precompile = false
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
