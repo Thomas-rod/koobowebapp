0f2ecd3103a7a2e00f416aa079421fa24e5a9f26
Roxane Haddad <roxane.haddad@gmail.com>
2020-04-17 15:48:47 +0200

    half info for flows index done

--- app/controllers/flows_controller.rb
+++ app/controllers/flows_controller.rb
@@ -1,5 +1,6 @@
 class FlowsController < ApplicationController
   def index
+    @flows = Flow.all.paginate(page: params[:page], per_page: 5).order(payment_date: :desc)
     @incomes = 0
     Flow.where(category: "income").each do |f|
       @incomes += f.amount
