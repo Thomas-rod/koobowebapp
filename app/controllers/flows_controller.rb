ff2a389962ea951635d1900a70caddef147dafeb
Roxane Haddad <roxane.haddad@gmail.com>
2020-04-17 17:13:44 +0200

    galère sur l'affichage des flows par appartement

--- app/controllers/flows_controller.rb
+++ app/controllers/flows_controller.rb
@@ -9,5 +9,12 @@ class FlowsController < ApplicationController
     Flow.where(category: "spending").each do |f|
       @spendings += f.amount
     end
+    @flat_flows = Flow.find_by_sql("
+      SELECT * FROM flows
+      JOIN rentings ON rentings.id = flows.renting_id
+      JOIN flats ON flats.id = rentings.flat_id
+      JOIN users ON users.id = flats.user_id
+      ORDER BY flows.payment_date desc
+      ")
   end
 end
