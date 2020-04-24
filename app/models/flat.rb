409685e20d35bce0ea6b190e1f522712cea48712
thomas rodier <rodiert17@gmail.com>
2020-04-24 15:11:16 +0200

    update

--- app/models/flat.rb
+++ app/models/flat.rb
@@ -5,9 +5,9 @@ class Flat < ApplicationRecord
   has_many :visits, through: :schedules, dependent: :destroy
   has_many :rentings, dependent: :destroy
   has_many :renting_folders, through: :rentings
-  has_many_attached :photos
   has_one_attached :technical_diagnostic
   has_one_attached :information_leaflet
   has_one_attached :co_owner_document
+  has_many_attached :photos
   validates :name, :address, :monthly_price, :number_of_rooms, :number_of_bedrooms, :surface, :floor, :heating_system, presence: true
 end
