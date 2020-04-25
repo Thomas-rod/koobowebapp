6b970a33fbabaa3ee16cc329657aad7f025fb1e4
thomas rodier <rodiert17@gmail.com>
2020-04-25 19:29:14 +0200

    add dashboard view and fix notif issue accross several pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,37 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
+  before_action :notif_visit, :notif_counter, only: [:dashboard]
 
   def home
+  end
+
+  def dashboard
+
+  end
+
+  def pricing
+  end
+
+  private
+
+  def notif_counter
+    @counter_appartement = 0
+    @counter_calendar = notif_visit.length
+    @counter_document = 0
+    @counter_accounting = 0
+    @counter_profil = 0
+    @counter = @counter_appartement + @counter_calendar + @counter_document + @counter_accounting + @counter_profil
+    return @counter
+  end
+
+  def notif_visit
+    pending_visit_all = Visit.where('status LIKE ?', 'pending')
+    visit_pending_flat = []
+    pending_visit_all.each do |visit|
+      if visit.schedule.flat.user.id == current_user.id
+       visit_pending_flat << visit
+      end
+    end
+    return visit_pending_flat
   end
 end
