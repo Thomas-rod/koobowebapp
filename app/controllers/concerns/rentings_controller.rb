879a849db0ead1797dd4ab79755070640dfdac04
Maodo Diop <maodod1@gmail.com>
2020-04-27 10:20:05 +0200

    first commit doc

--- app/controllers/concerns/rentings_controller.rb
+++ app/controllers/concerns/rentings_controller.rb
@@ -3,4 +3,19 @@ class RentingsController < ApplicationController
   def index
     @rentings = current_user.rentings
   end
+  def edit
+  @renting = Renting.find(params[:id])
+  end
+
+  def update
+    @renting = Renting.find(params[:id])
+    @renting.update(renting_params)
+    redirect_to rentings_path
+  end
+
+  private
+  def renting_params
+  params.require(:renting).permit( bails: [], etats_des_lieux: [], quittances: [])
+  end
 end
+
--- app/models/renting.rb
+++ app/models/renting.rb
@@ -4,6 +4,8 @@ class Renting < ApplicationRecord
   belongs_to :renting_folder
   has_many :messages
   has_many :flows, dependent: :destroy
-
+  has_many_attached :bails
+  has_many_attached :etats_des_lieux
+  has_many_attached :quittances
   validates :status, inclusion: { in: STATUS_RENTING }
 end
