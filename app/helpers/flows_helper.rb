19efa3d6f52d87d55b46ca59c6f22f9f34be4e94
Roxane Haddad <roxane.haddad@gmail.com>
2020-04-27 19:00:13 +0200

    added category to rent payed

--- app/helpers/flows_helper.rb
+++ app/helpers/flows_helper.rb
@@ -1,6 +1,6 @@
 module FlowsHelper
   def rent_payed?(flat)
-    @rent = flat.flows.find_by(month_rent: DateTime.now.mon)
+    @rent = flat.flows.find_by month_rent: DateTime.now.mon, category: "income"
     if @rent && @rent.amount == flat.monthly_price
       return true
     else
