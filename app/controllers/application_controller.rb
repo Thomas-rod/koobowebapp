7da20135041e2f0f8fb994c9dde77f5788385d9c
Roxane Haddad <roxane.haddad@gmail.com>
2020-04-29 12:33:45 +0200

    added default domain

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,9 @@
 class ApplicationController < ActionController::Base
   # app/controllers/application_controller.rb
 
-  # def default_url_options
-  #   { host: ENV["DOMAIN"] || "localhost:3000" }
-  # end
+  def default_url_options
+    { host: ENV["DOMAIN"] || "localhost:3000" }
+  end
 
   before_action :authenticate_user!
 
