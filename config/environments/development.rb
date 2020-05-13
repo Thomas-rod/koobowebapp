eedbe9981e1536a9f7e31beb153668db1e48bb32
thomas rodier <rodiert17@gmail.com>
2020-05-13 15:20:16 +0200

    update mailer with gmail

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -32,7 +32,7 @@ Rails.application.configure do
   config.active_storage.service = :amazon
 
   # Don't care if the mailer can't send.
-  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.delivery_method = :letter_opener
   config.action_mailer.default_url_options = { host: "http://localhost:3000" }
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.perform_caching = false
